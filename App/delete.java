e5ea98d8ce790b921254448f2f57f2d17a2c4956
mohamed musharraf <145838627+mrmusharraf@users.noreply.github.com>
2023-11-08 15:22:42 +0530

    Add files via upload

---
+++ App/delete.java
@@ -0,0 +1,39 @@
+package App;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+import App.FileHandling;
+import App.Menu;
+
+public class delete {
+    public static void delete(){
+        Scanner sc =new Scanner(System.in);
+        FileHandling.showClub();
+        System.out.print("Enter Club Name To delete : ");
+        String name=sc.nextLine();
+        try {
+            FileHandling.deleteClub(name);
+        } catch (IOException e) {
+            System.out.println("Error ");
+        }
+        System.out.print("Do you want to go again (y/n) :");
+        char c = sc.next().charAt(0);
+        switch (c) {
+            case 'y':
+                delete();
+                break;
+            case 'n':
+          //      Menu.mainMenu();
+                break;
+            default:
+                break;
+        }
+        sc.close();
+    }
+
+
+    public static void main(String[] args) {
+		delete.delete();
+	}
+}
